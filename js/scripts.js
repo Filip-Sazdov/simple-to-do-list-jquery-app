1cfea30ed178713fb9912c9697809577ef6701ca
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-10-24 20:57:04 -0400

    Added jQuery CDN

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-let container = $('.container');
+let container = $(".container");
 let inputSection = $('<div id ="input-section"></div>');
 let input = $('<input type="text" id = "input"></input>');
 let inputButton = $('<input type="submit" id = "button"></input>');
 
-
-let para = $('<p></p>');
-let list = $('<ol></ol>');
+let para = $("<p></p>");
+let list = $("<ol></ol>");
 
 para.append(list);
 inputSection.append(input);
@@ -15,15 +14,14 @@ container.append(inputSection);
 container.append(para);
 
 function addListItemToList() {
-	let inputValue = $('#input').val();
-  let li = $('<li class="list-item" ></li>');
-  li.text(inputValue);
-  list.append(li);
-	
+	let inputValue = $("#input").val();
+	let li = $('<li class="list-item" ></li>');
+	li.text(inputValue);
+	list.append(li);
 }
 inputButton.on("click", addListItemToList);
 
-let li = $('.list-item');
-li.on("click", function(){
+let li = $(".list-item");
+li.on("click", function () {
 	li.addClass("strike");
 });
