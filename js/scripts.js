8da1f77405e2142fe26b3a143330ffc8f526e6a7
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-10-25 13:50:29 -0400

    Added additional functionality, completed task

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
+//create html structure
 let container = $(".container");
 let inputSection = $('<div id ="input-section"></div>');
 let input = $(
-	'<input type="text" id = "input" placeholder="Please Type in a Task"></input>'
+	'<input type="text" id = "input" placeholder="Please Type in Task"></input>'
 );
 let inputButton = $('<button id = "button">Click to Enter Task</button>');
 let para = $("<p></p>");
@@ -14,22 +15,34 @@ inputSection.append(inputButton);
 container.append(inputSection);
 container.append(para);
 
+//add list item and give it functionality
 function addListItemToList() {
 	let inputValue = $("#input").val();
 	if (inputValue === "" || undefined) {
 		alert("You have not entered a task");
 	} else {
-		let li = $('<li class="list-item" ></li>');
-		li.text(inputValue);
+		let li = $('<li class="list-item" ></li>')
+			.text(inputValue)
+			.on("click", function () {
+				$(this).toggleClass("strike");
+			});
+		let span = $("<span>X<span>").on("click", function () {
+			li.addClass("delete");
+		});
+		li.append(span);
 		list.append(li);
 	}
+	//after inputting data, clear input box and give it back focus so user can continue typing
+	$("#input").val("").focus();
 }
 
-// inputButton.on("click", addListItemToList);
 inputButton.click(addListItemToList);
+//Use Enter key to input data
+$("#input").on("keyup", function (e) {
+	//13  means enter button
+	if (e.keyCode == 13) {
+		addListItemToList();
+	}
+});
 
-// let li = $(".list-item");
-// li.dblclick(function () {
-// 	console.log("clicked");
-// 	// li.addClass("strike");
-// });
+list.sortable();
